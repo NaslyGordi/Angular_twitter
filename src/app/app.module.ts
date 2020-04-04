ef8d88f55b4a1c3959e701faa3da68a9eabad8e4
gitorres1 <gitorres1@poligran.edu.co>
2020-04-04 14:40:14 -0500

    [master] Se crea el html del componente tweets y se corrigen errores con bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,10 +11,12 @@ import { TwetsComponent } from './twets/twets.component';
   declarations: [
     AppComponent,
     TwetsComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
