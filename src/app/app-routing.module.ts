6d88ee8634fec727e86ae0ad09f8e5c82e3231e0
Tatiana Cañón <55030034+TattaArias@users.noreply.github.com>
2020-04-10 12:01:52 -0500

    [TattaArias] Se carga nueva info

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListaTweetsComponent } from './lista-tweets/lista-tweets.component';
+import { CreateTweetComponent } from './create-tweet/create-tweet.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {   path: '', redirectTo: '/lista-Tweets', pathMatch: 'full'   },
+  {   path: 'lista-Tweets', component: ListaTweetsComponent   },
+  {   path: 'new-Tweets', component: CreateTweetComponent   },
+  {   path: '**', component: PageNotFoundComponent   }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
