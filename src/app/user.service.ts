a980296d2e482921e6a12fae56ef2df055c8ad9a
andres felipe <andres100r@hotmail.com>
2020-04-12 16:16:27 -0500

    user service

---
+++ src/app/user.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { User } from './models/user.model';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  ALL_USER: User[] = [
+    {   idUser: 1,   username: "Julian",      creation_date: "2020-04-10"     },
+    {   idUser: 2,   username: "Tatiana",  creation_date: "2020-04-10",    },
+    {   idUser: 3,   username: "Nasly",    creation_date: "2020-04-10",    },
+    {   idUser: 4,   username: "Katherin",  creation_date: "2020-04-10",    }
+  ]
+  constructor() { }
+  getAllUsers() : Observable<User[]>{
+    return of (this.ALL_USER);
+  }
+  createNewUser(newUser){    
+    this.ALL_USER.push(newUser);
+  }
+  
+}
+
+
