34331116490755387a1de1ffe461a443d5516e32
andres felipe <andres100r@hotmail.com>
2020-04-22 00:59:21 -0500

    delete usuario

--- src/app/delete-usuario/delete-usuario.component.ts
+++ src/app/delete-usuario/delete-usuario.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-delete-usuario',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./delete-usuario.component.css']
 })
 export class DeleteUsuarioComponent implements OnInit {
-
-  constructor() { }
-
+  d;
+  listUsers;
+  constructor(private UserService: UserService) { 
+    this.getAllUsers();
+  }
   ngOnInit(): void {
   }
 
+
+
+buscarUsuarioId(){
+const { idUser } = this.d ;
+const dato = this.listUsers.findOne({ where: { idUser: idUser }//Execute query
+});
+console.log("dato: " + dato);
 }
+
+getAllUsers(){
+  this.UserService.getAllUsers().subscribe(
+    listUserObs => {
+      this.listUsers = listUserObs;
+    }
+  
+    )
+  }
+  }
+
+
+
