ce94ad933544e3e6e93e891e2a0cdb1dc8c81643
Tatiana Cañón <55030034+TattaArias@users.noreply.github.com>
2020-04-24 17:46:48 -0500

    [TattaArias] Se carga información para búsqueda por autor pero ún no funciona :(

--- src/app/tweet-autor.service.ts
+++ src/app/tweet-autor.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Tweet } from './models/tweet.model';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TweetAutorService {
+  ALL_TWEETS: Tweet[] = [
+    {   id: 1,   texto: "Cualquie cosa",   autor: "@Julian",   fecha: "11Abr"   },
+]
+constructor( ) { }
+getAllTweets() : Observable<Tweet[]>{
+  return of (this.ALL_TWEETS);
+}
+buscarTweetAutor(buscar){
+  var index = this.ALL_TWEETS.findIndex(function (tweet){
+    return tweet.autor === buscar ;
+  })
 
-  constructor() { }
+}
+
 }
